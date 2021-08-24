6b548e677916c10e6a45fbc2333440f6b5dd0936
LewisMD3283 <lewismd3283@gmail.com>
2021-08-24 18:55:03 -0500

    fixed some spacing issues

--- Controllers/ChooseTeamController.cs
+++ Controllers/ChooseTeamController.cs
@@ -179,5 +179,18 @@ namespace PoGoBattleHelper.Controllers
             myModel.MyTeam = myTeam;
             return View(myModel);
         }
+
+        [HttpPost, Route("/ChooseMoves")]
+        public IActionResult ChooseMoves(string poke)
+        {
+            foreach (Pokemon mon in myTeam)
+            {
+                if (mon.Name == poke)
+                {
+                    //return a view that is dynamic to the pokemon and also shows all available moves in a checkbox form
+                }
+            }
+            return View();
+        }
     }
 }
